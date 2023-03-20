21a832c1eec21df6e040ee2f4609766a127c8280
Preston Button <prestonbutton99@gmail.com>
2023-03-20 11:17:46 -0400

    update packages and add extension code

--- extension.js
+++ extension.js
@@ -1,36 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.openChatGPT",
+    function () {
+      const panel = vscode.window.createWebviewPanel(
+        "ChatGPT",
+        "ChatGPT",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        }
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chatgptpb" is now active!');
+      panel.webview.html = `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>ChatGPT</title>
+        </head>
+        <body>
+          <iframe src="https://chat.openai.com/" width="100%" height="100%"></iframe>
+        </body>
+      </html>`;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatgptpb.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      panel.webview.onDidReceiveMessage(function (message) {
+        if (message.command === "logout") {
+          panel.dispose();
+        }
+      });
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from chatGPTpb!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
